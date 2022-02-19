f6bf780537f117ce2a60c7ef35776c8e5b208cae
Vlad <vladgrek616@gmail.com>
2022-02-20 00:41:26 +0200

    1. Name the project "Superhero" 2. Name the build schemes 3. Set your surname as the name of the application
    1. Name the project "Superhero"
    2. Name the build schemes
    3. Set your surname as the name of the application

---
+++ Superhero/ViewControllers/SupermanViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SupermanViewController.swift
+//  Superhero
+//
+//  Created by Vlad on 20.02.2022.
+//
+
+import UIKit
+
+
+class SupermanViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Superhero/ViewControllers/SuperwomanViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SuperwomanViewController.swift
+//  Superhero
+//
+//  Created by Vlad on 20.02.2022.
+//
+
+import UIKit
+
+class SuperwomanViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
